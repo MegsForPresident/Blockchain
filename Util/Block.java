638a0c7acf3d691c7242a670b958b51649a48314
Pramit Majumder <rhijusudipta@gmail.com>
2021-08-20 18:52:45 +0530

    Complete with no errors,yet

--- Util/Block.java
+++ Util/Block.java
@@ -8,13 +8,13 @@ public class Block {
     protected final int Phash;
     public final int hash;
     private Block Pblock;
+    
     public Block(String data,String date,int pHash,Block block) throws Util.Errors.GenesisBlockError{
         this.data = data;
         this.date = date;
         this.Phash = pHash;
         this.Pblock = block;
         if(Pblock != null){
-            System.out.println("yes "+Pblock);
             this.hash = new Hash(this.Pblock).hash();
         }
         else{
