969af03eb3a357ad95d14288bbcbb9b0966680d5
Pramit Majumder <rhijusudipta@gmail.com>
2021-08-23 14:15:27 +0530

    Complete

--- Util/Hash.java
+++ Util/Hash.java
@@ -1,14 +1,31 @@
 package Util;
 
-public class Hash{
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-    private Block Block;
-    
-    public Hash(Block block){
-        this.Block = block;
+public class Hash{
+    private final String data, date;
+    public Hash(Transformer block){
+        this.data = block.data;
+        this.date = block.date;
     }
 
-    public int hash(){
-        return Block.hash + 1;
+    protected String hash()
+    {
+        String dat = date + data + date;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD2");
+            byte[] messageDigest = md.digest(dat.getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
